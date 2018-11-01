35fd01bee2615c44237f9532666333a70697363e
ahb0327 <ahb0327@naver.com>
2018-11-02 01:50:58 +0900

    Fix router url

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/collectibles', collectiblesRouter);
+app.use('/api', indexRouter);
+app.use('/api/collectibles', collectiblesRouter);
 
 module.exports = app;
