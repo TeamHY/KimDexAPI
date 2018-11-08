93a9637280a178c7a11f1ac92e95360f335486fa
ahb0327 <ahb0327@naver.com>
2018-11-08 15:23:42 +0900

    Apply eslint

--- routes/collectibles.js
+++ routes/collectibles.js
@@ -1,22 +1,22 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const controller = require('../controllers/collectibles');
+const controller = require("../controllers/collectibles");
 
 /* GET users listing. */
 //router.get('/', function(req, res, next) {
 //  res.send('respond with a resource');
 //});
 
-router.get('/', (req, res) => {
-	res.send('검색어를 입력해주세요.')
+router.get("/", (req, res) => {
+  res.send("검색어를 입력해주세요.");
 });
 
-router.get('/:id', controller.show);
+router.get("/:id", controller.show);
 
-router.get('/upload', (req, res) => {
-	res.render('update')
+router.get("/upload", (req, res) => {
+  res.render("update");
 });
 
-router.post('/update', controller.update);
+router.post("/update", controller.update);
 
 module.exports = router;
