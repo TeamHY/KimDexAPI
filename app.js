5fc6935ccc7fd534a45e9937c140d7e5f3660699
ahb0327 <ahb0327@naver.com>
2018-11-21 01:01:48 +0900

    Add cors

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const indexRouter = require('./routes/index');
 const collectiblesRouter = require('./routes/collectibles');
@@ -16,6 +17,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors());
 
 app.use('/api', indexRouter);
 app.use('/api/collectibles', collectiblesRouter);
